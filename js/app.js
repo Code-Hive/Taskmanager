b8f5c39630c5d530bd027909d5ed739f0f9e903e
Sexcellent <seanhindle@Seans-MacBook-Pro.local>
2014-11-13 18:32:13 +0000

    Style updates

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 		{
 			'msg': 'This is a demo, to remove this one check "complete" below and then click clear.',
 			'for' : 'bill',
+			'type' : 'bug / error',
 			'priority' : 'low',
 			'date': '29121980',
 			'done': false
@@ -14,8 +15,12 @@
 	
 	// On form submit, add data from form to the controller
 		$scope.addtodo = function(){
-			$scope.todos.push({'msg':$scope.newtodomsg, 'priority':$scope.newtodopriority, 'date':$scope.newtododate, 'for':$scope.newtodofor,'done':false});
-			$scope.newtodo = '';
+			$scope.todos.push({
+			'msg':$scope.newtodomsg, 
+			'type':$scope.newtodotype, 
+			'priority':$scope.newtodopriority, 
+			'date':$scope.newtododate, 
+			'for':$scope.newtodofor,'done':false});c
 			$scope.newtodotitle = '';
 		};
 	// When 'clear' is clicked, filter the controller data and return results which are done is false
